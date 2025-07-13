fb6d2d759a04a78aea2af1536e0bb8f3d6af1872
colloid <jack0904h3@yahoo.co.jp>
2025-07-13 18:03:04 +0900

    Added functionality to allow streaming and RPC selection from PlayerSpawner

--- scripts/PlayerSpawner.cs
+++ scripts/PlayerSpawner.cs
@@ -7,12 +7,22 @@ public class PlayerSpawner : NetworkBehaviour, IPlayerJoined
 {
     [Header("Avatar Configuration")]
     [SerializeField] private NetworkPrefabRef PlayerPrefab;
+    [SerializeField] private SyncMode syncMode = SyncMode.Inharitence; // Default to Inherit from RMCprotocol
+
+    private enum SyncMode
+    {
+        Inharitence = -1, // Inherit from RMCprotocol
+        RPC = 0,
+        Streaming = 1
+    }
     
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
-            Runner.Spawn(PlayerPrefab, Vector3.up, Quaternion.identity);
+            var playerObject = Runner.Spawn(PlayerPrefab, Vector3.up, Quaternion.identity);
+            if (syncMode >= 0)playerObject.GetComponent<RMCprotocol>().SelectedSyncMode = (RMCprotocol.SyncMode)(int)syncMode; // Set the sync mode for the local player
+
             Debug.Log($"Local avatar spawned for player {player}");
         }
     }
