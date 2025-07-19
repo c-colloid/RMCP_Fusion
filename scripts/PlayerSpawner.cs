2e038f60fd8de139b6404e6ca32e150ba8e10311
colloid <jack0904h3@yahoo.co.jp>
2025-07-19 15:29:31 +0900

    Added the ability to automatically register spawn events to Network Runner

--- scripts/PlayerSpawner.cs
+++ scripts/PlayerSpawner.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using Fusion;
 using UnityEngine;
+using Meta.XR.MultiplayerBlocks.Fusion;
 
-public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
+public class PlayerSpawner : SimulationBehaviour, IDespawned, IPlayerJoined
 {
     [Header("Avatar Configuration")]
     [SerializeField] private NetworkPrefabRef PlayerPrefab;
     
+    private void Awake()
+    {
+        FusionBBEvents.OnConnectedToServer += RegisterOnRunner;
+    }
+
+    public void RegisterOnRunner(NetworkRunner runner)
+    {
+        // var runner = NetworkRunner.GetRunnerForGameObject(gameObject);
+        if (runner.IsRunning)
+        {
+            runner.AddGlobal(this);
+        }
+    }
+    
+    public void Despawned(NetworkRunner runner, bool hasState)
+    {
+        // var runner = NetworkRunner.GetRunnerForGameObject(gameObject);
+        if (runner.IsRunning)
+        {
+            runner.RemoveGlobal(this);
+        }
+    }
+
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
-            Runner.Spawn(PlayerPrefab, Vector3.up, Quaternion.identity);
+            Runner.Spawn(PlayerPrefab, Vector3.zero, Quaternion.identity);
             Debug.Log($"Local avatar spawned for player {player}");
         }
     }
